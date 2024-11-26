cd11e0dc8951c1ab8d6fc5369d912b5bcaac6457
Rahul Salve <salverahul367@gmail.com>
2024-11-26 13:22:04 +0530

    commited all files

---
+++ Myntra Clone/script/indext.js
@@ -0,0 +1,28 @@
+
+function displayItemsOnHomePage(){
+    let itemsContainerElement = document.querySelector('.items-container');
+
+let innerHtml = '';
+items.forEach(item =>{
+    innerHtml += `
+        <div class="item-container">
+                <img class="item-image" src="${item.image}" alt="image">
+
+                <div class="rating">${item.rating.stars}⭐ | ${item.rating.count}</div>
+
+                <div class="company-name">${item.company}</div>
+
+                <div class="item-name">${item.item_name}</div>
+
+                <div class="price">
+                    <span class="current-price">Rs ${item.current_price}</span>
+                    <span class="original-price">Rs ${item.original_price}</span>
+                    <span class="discount">(${item.discount_percentage}% OF)</span>
+                </div>
+                <button class="btn-add-to-bag">Add To Bag</button>
+         </div>`
+});
+
+itemsContainerElement.innerHTML = innerHtml;
+}
+displayItemsOnHomePage()
